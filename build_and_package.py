b21c2881b4af4f4addfd512343f27d50fb5b260a
apnorton <hskrus@gmail.com>
2024-02-01 23:48:03 -0500

    Added a package script to wrap generated wasm in js code.

---
+++ build_and_package.py
@@ -0,0 +1,55 @@
+import base64
+import os
+import sys
+import shutil
+import subprocess
+from pathlib import Path
+
+from jinja2 import Template
+
+if len(sys.argv) < 2:
+    print("USAGE: python build_and_package.py [project_folder]")
+    sys.exit(1)
+
+project = Path(sys.argv[1])
+os.chdir(project)
+
+target_folder = Path("./pkg/")
+
+# first do a little cleanup
+shutil.rmtree(target_folder)
+
+# Build the js (has exports, tho, so we'll do some sketchy substitution)
+subprocess.run(["wasm-pack", "build", "--release", "--target", "web"])
+
+potential_wasm_files = list(target_folder.glob("*_bg.wasm"))
+assert len(potential_wasm_files) == 1
+
+wasm_file = potential_wasm_files[0]
+with open(wasm_file, "rb") as f:
+    wasm_data = f.read()
+
+wasm_encoded = base64.b64encode(wasm_data).decode("utf-8")
+
+potential_js_files = list(target_folder.glob("*.js"))
+assert len(potential_js_files) == 1
+
+js_file = potential_js_files[0]
+with open(js_file) as f:
+    js_lines = f.read().splitlines()
+
+for i in range(len(js_lines)):
+    line = js_lines[i]
+    if line.strip().startswith("export function"):
+        js_lines[i] = line.replace("export ", "")
+    elif line.strip().startswith("export"):
+        js_lines[i] = "//delete this"
+js_lines = filter(lambda s: s != "//delete this", js_lines)
+
+os.chdir("..")
+with open("bot_template.js.j2") as f:
+    template = Template(f.read())
+
+rendered = template.render(wasm_encoded=wasm_encoded, generated_js="\n".join(js_lines))
+with open("bot_code.js", "w") as f:
+    f.write(rendered)
